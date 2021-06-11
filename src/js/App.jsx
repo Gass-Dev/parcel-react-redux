9d1d47741ee6db959bd80e3e2f42e6527a1ed7f4
Gass-Dev <madebygass@gmail.com>
2021-06-11 11:46:50 +0200

    [Register] Added some error logic along with a loading state

---
+++ src/js/App.jsx
@@ -0,0 +1,8 @@
+//Motherlord
+import React from "react";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
+
+export const App = () => {
+  return (<Register />), (<Login />);
+};
---
+++ src/js/pages/Register.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { InputGroup } from "../components/inputGroupe";
+import { SubmitButton } from "../components/submitButton";
+import api from "../../utils/api";
+
+//High Order Component
+export const Register = () => {
+  // let email = ""
+  // const setEmail = (value) => {
+  //     email = value;
+  // }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [fieldError, setFieldError] = useState("");
+  const [isLoading, setIsLoading] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError(null);
+    setFieldError(null);
+    setIsLoading(false);
+
+    console.log("Register state : Email -> ", email);
+    console.log("Register state : Password -> ", password);
+
+    let body = {
+      email,
+      password,
+    };
+
+    // const result = await fetch("http://localhost:4000/api/v1/users/", {
+    //   method: "POST",
+    //   body: JSON.stringify(body),
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // });
+
+    // console.log("result", result);
+
+    try {
+      setIsLoading(true);
+      const axiosResult = await api.post("users/", { email, password });
+      console.log("result with an axios method", axiosResult);
+      setIsLoading(false);
+    } catch (err) {
+      console.log("axios err :", err.response);
+      setError(err.respons?.data?.message);
+      setFieldError(err.respons?.data?.field);
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div>
+      <h1>Register</h1>
+      <p style={{ color: "red" }}>{error && error}</p>
+      {isLoading && <p>Loading...</p>}
+      <form onSubmit={handleSubmit}>
+        <InputGroup
+          handleChange={setEmail}
+          isValid={fieldError !== "email"}
+          label="Email"
+          type="email"
+          required
+        />
+        <InputGroup
+          handleChange={setPassword}
+          isValid={fieldError !== "password"}
+          label="Password"
+          type="password"
+          required={true}
+          minLength="1"
+          maxLength="15"
+        />
+        <SubmitButton name="S'enregistrer" />
+      </form>
+    </div>
+  );
+};
